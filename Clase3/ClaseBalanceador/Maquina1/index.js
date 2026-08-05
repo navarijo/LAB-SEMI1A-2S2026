513c413fd7a3d8fa59b76e518b6a92c3a3e8282b
Elder Navarijo <3672337111006@ingenieria.usac.edu.gt>
2026-08-05 16:07:02 -0600

    Clase 3: Balanceador de cargas

---
+++ Clase3/ClaseBalanceador/Maquina1/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+var fs = require("fs");
+
+app.get('/listUsers', function (req, res) {
+   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+      console.log( data );
+      res.end( data );
+   });
+})
+
+app.get('/', (req, res) => {
+   res.status(200).send('OK');
+ });
+
+var server = app.listen(3000, function () {
+   var host = server.address().address
+   var port = server.address().port
+   console.log(`Se está ejecutando en el puerto: http://${host}:${port}`);
+
+})
